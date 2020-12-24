e989e903f905ef7a0ebf53a5a684f2ca1826439e
Jeff Johnston <jeffwjohn@yahoo.com>
2020-12-24 13:39:42 -0600

    add mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,24 @@
 const { User, Thought } = require("../models");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            .populate('thoughts')
+            .populate('friends');
+      
+          return userData;
+        }
+      
+        throw new AuthenticationError('Not logged in');
+      },
+      
+
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
       // Here, we pass in the parent as more of a placeholder parameter. It won't be used, but we need something in that first parameter's spot so we can access the username argument from the second parameter. We use a ternary operator to check if username exists. If it does, we set params to an object with a username key set to that value. If it doesn't, we simply return an empty object. We then pass that object, with or without any data in it, to our .find() method. If there's data, it'll perform a lookup by a specific username. If there's not, it'll simply return every thought.
@@ -27,6 +44,72 @@ const resolvers = {
         .populate("thoughts");
     },
   },
+  Mutation: {
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+      
+        return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+      
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const correctPw = await user.isCorrectPassword(password);
+      
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const token = signToken(user);
+        return { token, user };
+      },
+      addThought: async (parent, args, context) => {
+        if (context.user) {
+          const thought = await Thought.create({ ...args, username: context.user.username });
+      
+          await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { thoughts: thought._id } },
+            { new: true }
+          );
+      
+          return thought;
+        }
+      
+        throw new AuthenticationError('You need to be logged in!');
+      },
+      addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+        if (context.user) {
+          const updatedThought = await Thought.findOneAndUpdate(
+            { _id: thoughtId },
+            { $push: { reactions: { reactionBody, username: context.user.username } } },
+            { new: true, runValidators: true }
+          );
+      
+          return updatedThought;
+        }
+      
+        throw new AuthenticationError('You need to be logged in!');
+      },
+      addFriend: async (parent, { friendId }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $addToSet: { friends: friendId } },
+            // This mutation will look for an incoming friendId and add that to the current user's friends array. A user can't be friends with the same person twice, though, hence why we're using the $addToSet operator instead of $push to prevent duplicate entries
+            { new: true }
+          ).populate('friends');
+      
+          return updatedUser;
+        }
+      
+        throw new AuthenticationError('You need to be logged in!');
+      }
+  }
 };
 
 module.exports = resolvers;

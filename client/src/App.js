64df40ce1cf7751edfc259109cde10bb861038b7
Jeff Johnston <jeffwjohn@yahoo.com>
2020-12-25 12:23:54 -0600

    add react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React from "react";
 
 // add these two library import statements
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 // So we just imported two key pieces to the application. The first, ApolloProvider, is a special type of React component that we'll use to provide data to all of the other components. We'll use the second, ApolloClient, to get that data when we're ready to use it.
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// BrowserRouter and Route are components that the React Router library provides. We renamed BrowserRouter to Router to make it easier to work with.
 
-import Home from './pages/Home';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
 const client = new ApolloClient({
-  uri: '/graphql'
+  uri: "/graphql",
 });
-
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
 
 export default App;
+
+// The ? means this parameter is optional, so /profile and /profile/myUsername will both render the Profile component. Later on, we'll set up /profile to display the logged-in user's information.
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_THOUGHT } from "../utils/queries";
+import ReactionList from '../components/ReactionList';
 
-const SingleThought = props => {
+const SingleThought = (props) => {
+  const { id: thoughtId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId },
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  // This is similar to the query logic that you used on the homepage. The variables loading and data are destructured from the useQuery Hook. The loading variable is then used to briefly show a loading <div> element, and the data variable is used to populate a thought object. There is one difference, however. The useQuery Hook was given a second argument in the form of an object. This is how you can pass variables to queries that need them. The id property on the variables object will become the $id parameter in the GraphQL query.
+
   return (
-    <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            Username
-          </span>{' '}
-          thought on createdAt
-        </p>
-        <div className="card-body">
-          <p>Thought Text</p>
-        </div>
-      </div>
+<div>
+  <div className="card mb-3">
+    <p className="card-header">
+      <span style={{ fontWeight: 700 }} className="text-light">
+        {thought.username}
+      </span>{' '}
+      thought on {thought.createdAt}
+    </p>
+    <div className="card-body">
+      <p>{thought.thoughtText}</p>
     </div>
+  </div>
+
+  {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+</div>
+
   );
 };
 

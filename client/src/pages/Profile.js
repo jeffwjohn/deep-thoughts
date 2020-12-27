1343fa4f82794561d41a9c798a8be740cc4446f5
Jeff Johnston <jeffwjohn@yahoo.com>
2020-12-27 11:47:04 -0600

    add ThoughtForm component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,11 +3,13 @@ import React from "react";
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
 
-import { useQuery } from "@apollo/react-hooks";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import Auth from "../utils/auth";
 import { Redirect, useParams } from "react-router-dom";
 // This component, Redirect, will allow us to redirect the user to another route within the application. Think of it like how we've used location.replace() in the past, but it leverages React Router's ability to not reload the browser!
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -18,6 +20,8 @@ const Profile = () => {
   });
 
   const user = data?.me || data?.user || {};
+
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   // redirect to personal profile page if username is the logged-in user's
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -36,14 +40,32 @@ const Profile = () => {
       </h4>
     );
   }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };  
   
   return (
     <div>
-      <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : "your"} profile.
-        </h2>
-      </div>
+<div className="flex-row mb-3">
+  <h2 className="bg-dark text-secondary p-3 display-inline-block">
+    Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+  </h2>
+
+  {userParam && (
+  <button className="btn ml-auto" onClick={handleClick}>
+    Add Friend
+  </button>
+)}
+
+</div>
+
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
@@ -61,6 +83,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
